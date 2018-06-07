d9424461e9f5b2bc2763c7ce511ea5334b90c2f9
oliyg <billyangg@qq.com>
2018-06-07 10:23:03 +0800

    [new] favicon bugfixed ejs template

--- src/app.js
+++ src/app.js
@@ -6,17 +6,22 @@ const stat = promisify(fs.stat)
 const readDir = promisify(fs.readdir)
 const path = require('path')
 const chalk = require('chalk') // terminal string styling
+const ejs = require('ejs')
 // require self modules
 const autoOpenUrl = require('./utils/autoOpenUrl')
 const printLog = require('./utils/printLog')
 // require file
 const config = require('./config')
+// template
+const tplPath = path.join(__dirname, './template/index.tpl.ejs')
+const source = fs.readFileSync(tplPath, { encoding: 'UTF-8' })
+const template = ejs.compile(source.toString())
 
 const server = http.createServer(async (req, res) => {
   
   printLog(req) // print req and res log to terminal
 
-  const filePath = path.join(config.root, req.url)
+  const filePath = path.join(config.root, req.url) // !file absolute path
   try {
     const stats = await stat(filePath)
 
@@ -28,8 +33,13 @@ const server = http.createServer(async (req, res) => {
     } else if (stats.isDirectory()) {
       const dir = await readDir(filePath) // read directory
       res.statusCode = 200
-      res.setHeader('Content-Type', 'text/plain')
-      res.end(dir.join('\n'))
+      res.setHeader('Content-Type', 'text/html')
+      const data = {
+        title: path.basename(filePath) || '/',
+        dir: path.relative(process.cwd(), filePath),
+        files: dir
+      }
+      res.end(template(data))
     }
 
   } catch (error) {
